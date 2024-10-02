cb07b010b3877481ace93ed05c64d2c3f1d0d8e5
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-10-02 10:39:52 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter x1: ");
+        String x1String = s.nextLine();
+        int x1 = Integer.parseInt(x1String);
+
+        System.out.println();
+
+        System.out.print("Enter x2: ");
+        String x2String = s.nextLine();
+        int x2 = Integer.parseInt(x2String);
+
+        System.out.println();
+
+        System.out.print("Enter y1: ");
+        String y1String = s.nextLine();
+        int y1 = Integer.parseInt(y1String);
+
+        System.out.println();
+
+        System.out.print("Enter y2: ");
+        String y2String = s.nextLine();
+        int y2 = Integer.parseInt(y2String);
+
+        System.out.println();
+
+        LinearEquation line1 = new LinearEquation(x1, x2, y1, y2);
+
+        System.out.println(line1.toString());
+
+        System.out.println();
+
+        System.out.print("Enter x3: ");
+        String x3String = s.nextLine();
+        double x3 = Double.parseDouble(x3String);
+
+        line1.thirdValue(x3);
+        System.out.println(line1.thirdValuePrint(x3));
     }
 }
